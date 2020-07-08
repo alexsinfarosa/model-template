8b610c4891e81c888572dfc2efece20ee43eba3c
alexsinfarosa <as898@cornell.edu>
2020-07-08 15:16:27 +0200

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,23 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex flex-col h-screen justify-center items-center">
+      <p>You just hit a route that doesn&#39;t exist.</p>
+
+      <button
+        type="button"
+        className="inline-flex items-center p-2 sm:px-3 sm:py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-secondary-600 hover:bg-secondary-500 focus:outline-none focus:border-secondary-700 focus:shadow-outline-secondary active:bg-secondary-700 transition ease-in-out duration-150"
+      >
+        <Link to="/blueberry-maggot" className="no-underline text-white">
+          Return to Homepage
+        </Link>
+      </button>
+    </div>
   </Layout>
 )
 
